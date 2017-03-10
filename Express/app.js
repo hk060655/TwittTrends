3d5287a185400dc59763a4b7a2a5f28fce69b7b9
Kai He <kh2789@columbia.edu>
2017-03-09 21:50:36 -0500

    update express

--- Express/app.js
+++ Express/app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+var Elasticsearch = require('aws-es');
+var bodyParser = require('body-parser');
 
 app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
    res.send('Hello World');
@@ -14,7 +18,49 @@ app.get('/index', function (req, res) {
 
 app.post('/display', function (req, res) {
   console.log("Request handler Display");
-  res.render('display');
+  console.log("Parsed: " + req.body.selection);
+    var result = [];
+
+    elasticsearch = new Elasticsearch({
+        accessKeyId: 'AKIAJ7OSXNTJ2ZYVZ5XQ',
+        secretAccessKey: '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',
+        service: 'es',
+        region: 'us-east-1',
+        host: 'search-test-off63ohnto3svkei2nbfv3oyj4.us-east-1.es.amazonaws.com'
+    });
+
+
+    elasticsearch.search({
+        index: 'tweets_test',
+        type: 'tweets',
+        body: {
+            query: {
+                match: { "text": "great" }
+            },
+        }
+    },function (error, response,status) {
+        if (error){
+            console.log("search error: "+error);
+        }
+        else {
+            console.log("--- Response ---");
+            console.log("--- Hits ---");
+            response.hits.hits.forEach(function(hit){
+                console.log(hit._source.user.location);
+                console.log(typeof hit._source.user.location);
+                result.push(hit._source.user.location);
+            })
+        }
+        console.log("result = " + result);
+        res.render('display', {locs: result});
+    });
+
+    // console.log(searched);
+    // console.log(typeof searched);
+
+    // console.log(result);
+    // res.send(result);
+    // res.render('display');
 })
 
 var server = app.listen(3000, function () {
